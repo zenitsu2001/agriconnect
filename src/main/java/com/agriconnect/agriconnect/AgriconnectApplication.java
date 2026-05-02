a728b548e5888fa1aaacd4fd8f39b3377d7691d4
pape11 <papeboubacardiallo2@gmail.com>
2026-05-02 13:06:06 +0000

    Initialisation du projet

---
+++ src/main/java/com/agriconnect/agriconnect/AgriconnectApplication.java
@@ -0,0 +1,13 @@
+package com.agriconnect.agriconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriconnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriconnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agriconnect/agriconnect/AgriconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agriconnect.agriconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriconnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
